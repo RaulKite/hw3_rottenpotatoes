71b9c7301efebc2dd4f66146c0db91dbeb949872
Raul Sanchez <raul@um.es>
2012-03-13 09:28:14 +0100

    avoid to include .redcar dir in git

--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -2,6 +2,8 @@
 
 Given /the following movies exist/ do |movies_table|
   movies_table.hashes.each do |movie|
+    peli = Movie.new
+    
     # each returned element will be a hash whose key is the table header.
     # you should arrange to add that movie to the database here.
   end
